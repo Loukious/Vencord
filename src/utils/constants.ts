e9def6d134f62df623bd2e8360f79c7ae9b3fb64
Loukious <Loukious@users.noreply.github.com>
2026-04-23 20:54:11 +0100

    feat(constants): add new developer entry for 'milas'

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -669,6 +669,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    milas: {
+        name: "milas",
+        id: 264728985114640385n,
+    },
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
